ae74e1ee57c546d64a5d83905e82d469b35f9f5f
Fernando <fcam2202@gmail.com>
2023-10-06 18:15:42 -0500

    Subida de archivos

---
+++ script.js
@@ -0,0 +1,15 @@
+let card1 = document.getElementById("1")
+let card2 = document.getElementById("2")
+let card3 = document.getElementById("3")
+let card4 = document.getElementById("4")
+let card5 = document.getElementById("5")
+
+card1.addEventListener("click",function(){
+    window.location.href = "culturalight.html"
+})
+card2.addEventListener("click",function(){
+    window.location.href = "culturadescarte.html"
+})
+card3.addEventListener("click",function(){
+    window.location.href = "desafios.html"
+})
